c6cd9e4fb779f2363e0a09ec52bcf9b2261f017b
khalid-alsahafi <khallshock@gmail.com>
2025-05-01 17:00:59 +0300

    Renamed all html files to cshtml\nadded Users class and UserDbContext

---
+++ CPIS-358_Project/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace CPIS_358_Project.Models
+{
+    public class User
+    {
+        public string username { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public int highestScore { get; set; }
+        public int lastScore { get; set; }
+    }
+}
---
+++ CPIS-358_Project/Models/UserDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+namespace CPIS_358_Project.Models
+
+{
+    public class UserDbContext : IdentityDbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
+        public DbSet<User> Users {  get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
